36ed8fc0942331c176adca5df9bd881d9ad43732
Jacky <jackygilbertson@users.noreply.github.com>
2017-08-15 14:33:51 -0700

    Testing!

--- assets/tpnd/js/killHover.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    var isTouchDevice = 'ontouchstart' in document.documentElement;
-
-    if (isTouchDevice == true) {
-        $('html').addClass('is-touch');
-    } else {
-        $('html').addClass('not-touch');
-    }
-});
